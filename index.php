34b7930a959cd1f33d1b3ebbad8b78b1b17faf46
alireza alavi <alavialireza38@gmail.com>
2022-05-27 15:11:45 +0430

    start php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="contact.css">
+    <title>تماس با من - علیرضا علوی</title>
+</head>
+<body dir="rtl">
+    <br>
+    <br>
+    <br>
+    <h2>سلام و عرض خوش آمد!</h2>
+    <br>
+    <h5>راه های ارتباطی با من</h5>
+    <br>
+    <div class="d-grid gap-3 col-8 col-lg-4 mx-auto">
+        <a href="https://alialavii.ir/" class="btn border-primary" type="button" target="_blank">سایت</a>
+        <a href="https://instagram.com/programming._.fun" class="btn border-primary" type="button" target="_blank">اینستاگرام</a>
+        <a href="https://twitter.com/alrez81" class="btn border-primary" type="button" target="_blank">توییتر</a>
+        <a href="https://www.linkedin.com/in/alireza-alavi-83389822b/" class="btn border-primary" type="button" target="_blank">لینکدین</a>
+        <a href="https://github.com/alireza8686" class="btn border-primary" type="button" target="_blank">گیت هاب</a>
+    </div>
+    <p class="d-grid gap-3 col-5 col-lg-4 mx-auto">
+        <h6>ارتباط سریع:</h6>
+        <a href="https://api.whatsapp.com/send?phone=989117970185" class="btn border-primary" type="button" target="_blank" id="wt">واتساپ</a>
+        <a href="https://t.me/aliwalv" class="btn border-primary" type="button" target="_blank" id="wt">تلگرام</a>
+    </p>
+
+    <script src="contact.js">
+    </script>
+</body>
+</html>
