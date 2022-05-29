1349b61e4f29ae425c3d0ad030602d26bcf0210c
alireza alavi <alavialireza38@gmail.com>
2022-05-29 12:30:04 +0430

    refactore 1

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,37 +8,34 @@
     <link rel="stylesheet" href="contact.css">
     <title>تماس با من - علیرضا علوی</title>
 </head>
-<?php $color = ""; ?>
-<?php switch($_POST["ss"]){
-    case "green":
-        $body_text_color = "344e41";
-        $btn_text_color = "white";
-        $background = "dad7cd";
-        $btn_background= "588157";
-        $btn_hover = "#283618";
-        break;
-    case "blue":
-        $body_text_color = "blue";
-        $btn_text_color = "white";
-        $background = "caf0f8";
-        $btn_background= "023e8a";
-        $btn_hover = "#03045e";
-        break;
-    case "red":
-        $body_text_color = "003049";
-        $btn_text_color = "white";
-        $background = "fdf0d5";
-        $btn_background= "c1121f";
-        $btn_hover = "#780000";
-        break;
-    default:
-        $body_text_color = "344e41";
-        $btn_text_color = "white";
-        $background = "dad7cd";
-        $btn_background= "588157";
-        break;
-}
+
+<?php   if ($_POST){
+            switch($_POST["color"]){
+                case "green":
+                    $background = "dad7cd";
+                    $btn_background= "588157";
+                    $btn_hover = "#283618";
+                    break;
+                case "blue":
+                    $background = "caf0f8";
+                    $btn_background= "023e8a";
+                    $btn_hover = "#03045e";
+                    break;
+                case "red":
+                    $background = "fdf0d5";
+                    $btn_background= "c1121f";
+                    $btn_hover = "#780000";
+                    break;
+                }
+            }
+
+        else {
+            $background = "dad7cd";
+            $btn_background= "588157";
+            $btn_hover = "#283618";
+        }
 ?>
+
 <style>
     a:link {
         background-color: #<?php echo $btn_background;?>;
@@ -46,12 +43,12 @@
     a:hover{
         background-color: <?php echo $btn_hover;?>;
     }
-        #wt:hover{
+    #wt:hover{
         background-color: <?php echo $btn_hover;?>;
     }
 </style>
-<body dir="rtl" style="color:#<?php echo $body_text_color;?>; background-color:#<?php echo $background; ?>;">
-    <?php echo $color; ?>
+
+<body dir="rtl" style="color:black ; background-color:#<?php echo $background; ?>;">
     <br>
     <br>
     <h2>سلام و عرض خوش آمد!</h2>
@@ -59,25 +56,25 @@
     <h5>راه های ارتباطی با من</h5>
     <br>
     <div class="d-grid gap-3 col-8 col-lg-4 mx-auto">
-        <a href="https://alialavii.ir/" class="btn" type="button" target="_blank" style="color:<?php echo $btn_text_color;?>;">سایت</a>
-        <a href="https://instagram.com/programming._.fun" class="btn" type="button" target="_blank" style="color:<?php echo $btn_text_color;?>;">اینستاگرام</a>
-        <a href="https://twitter.com/alrez81" class="btn" type="button" target="_blank" style="color:<?php echo $btn_text_color;?>;">توییتر</a>
-        <a href="https://www.linkedin.com/in/alireza-alavi-83389822b/" class="btn" type="button" target="_blank" style="color:<?php echo $btn_text_color;?>;">لینکدین</a>
-        <a href="https://github.com/alireza8686" class="btn" type="button" target="_blank" style="color:<?php echo $btn_text_color;?>;">گیت هاب</a>
+        <a href="https://alialavii.ir/" class="btn" type="button" target="_blank" style="color:white;">سایت</a>
+        <a href="https://instagram.com/programming._.fun" class="btn" type="button" target="_blank" style="color:white;">اینستاگرام</a>
+        <a href="https://twitter.com/alrez81" class="btn" type="button" target="_blank" style="color:white;">توییتر</a>
+        <a href="https://www.linkedin.com/in/alireza-alavi-83389822b/" class="btn" type="button" target="_blank" style="color:white;">لینکدین</a>
+        <a href="https://github.com/alireza8686" class="btn" type="button" target="_blank" style="color:white;">گیت هاب</a>
     </div>
     <p class="d-grid gap-3 col-5 col-lg-4 mx-auto">
         <h6>ارتباط سریع:</h6>
-        <a href="https://api.whatsapp.com/send?phone=989117970185" class="btn" type="button" target="_blank" id="wt" style="color:<?php echo $btn_text_color;?>;">واتساپ</a>
-        <a href="https://t.me/aliwalv" class="btn" type="button" target="_blank" id="wt" style="color:<?php echo $btn_text_color;?>;">تلگرام</a>
+        <a href="https://api.whatsapp.com/send?phone=989117970185" class="btn" type="button" target="_blank" id="wt" style="color:white;">واتساپ</a>
+        <a href="https://t.me/aliwalv" class="btn" type="button" target="_blank" id="wt" style="color:white;">تلگرام</a>
     </p>
     <hr>
     <p>اگه میخوای تم اینجا تغییر کنه یکی رو انتخاب کن:</p>
-    <form action="" method="POST">
-        <input type="radio" value="green" name="ss" checked="checked">سبز ---
+    <form method="POST">
+        <input type="radio" value="green" name="color" checked="checked">سبز ---
 
-        <input type="radio" value="blue" name="ss">آبی ---
+        <input type="radio" value="blue" name="color">آبی ---
 
-        <input type="radio" value="red" name="ss">قرمز
+        <input type="radio" value="red" name="color">قرمز
         <br>
         <br>
         <input type="submit" value="حالا اینجا کلیک کن!" id="click">
