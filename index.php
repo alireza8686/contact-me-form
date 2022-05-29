681935e130bb8653453af95ec92b205707145d66
alireza alavi <alavialireza38@gmail.com>
2022-05-29 13:37:54 +0430

    front style

--- index.php
+++ index.php
@@ -46,14 +46,37 @@
     #wt:hover{
         background-color: <?php echo $btn_hover;?>;
     }
+    #theme{
+        color:white;
+        line-height : 80%;
+        background-color: #<?php echo $btn_background;?>;
+        border-radius: 20px;
+    }
+    #conect{
+        line-height : 50%;
+    }
 </style>
 
 <body dir="rtl" style="color:black ; background-color:#<?php echo $background; ?>;">
     <br>
-    <br>
     <h2>سلام و عرض خوش آمد!</h2>
+    <div class="col-6 col-lg-4 mx-auto border border-3 " id="theme" style="padding:12px;">
+    <p style="font-size:15px;  ">میتونی تم صفحه رو تغییر بدی!</p>
+    <p style="font-size:15px;">اول یکی رو انتخاب کن</p>
+    <form method="POST">
+        <input type="radio" value="green" name="color" checked="checked">سبز ---
+
+        <input type="radio" value="blue" name="color">آبی ---
+
+        <input type="radio" value="red" name="color">قرمز
+        <br>
+        <br>
+        <input type="submit" value="حالا اینجا کلیک کن!" id="click" style="padding:10px;line-height : 20%;">
+    </form>
+    </div>
     <br>
-    <h5>راه های ارتباطی با من</h5>
+    <div id="conect">
+    <h5>راه های ارتباطی با من :</h5>
     <br>
     <div class="d-grid gap-3 col-8 col-lg-4 mx-auto">
         <a href="https://alialavii.ir/" class="btn" type="button" target="_blank" style="color:white;">سایت</a>
@@ -67,19 +90,7 @@
         <a href="https://api.whatsapp.com/send?phone=989117970185" class="btn" type="button" target="_blank" id="wt" style="color:white;">واتساپ</a>
         <a href="https://t.me/aliwalv" class="btn" type="button" target="_blank" id="wt" style="color:white;">تلگرام</a>
     </p>
-    <hr>
-    <p>اگه میخوای تم اینجا تغییر کنه یکی رو انتخاب کن:</p>
-    <form method="POST">
-        <input type="radio" value="green" name="color" checked="checked">سبز ---
-
-        <input type="radio" value="blue" name="color">آبی ---
-
-        <input type="radio" value="red" name="color">قرمز
-        <br>
-        <br>
-        <input type="submit" value="حالا اینجا کلیک کن!" id="click">
-    </form>
-    <hr>
+    </div>
     <script src="contact.js">
     </script>
 </body>
