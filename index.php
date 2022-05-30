174562f42269e9a6f3f066a28f618c4bedc83637
alireza alavi <alavialireza38@gmail.com>
2022-05-30 12:06:47 +0430

    top circle

--- index.php
+++ index.php
@@ -15,16 +15,19 @@
                     $background = "dad7cd";
                     $btn_background= "588157";
                     $btn_hover = "#283618";
+                    $top_circle = "img/11.png";
                     break;
                 case "blue":
                     $background = "caf0f8";
                     $btn_background= "023e8a";
                     $btn_hover = "#03045e";
+                    $top_circle = "img/12.png";
                     break;
                 case "red":
                     $background = "fdf0d5";
                     $btn_background= "c1121f";
                     $btn_hover = "#780000";
+                    $top_circle = "img/13.png";
                     break;
                 }
             }
@@ -33,6 +36,7 @@
             $background = "dad7cd";
             $btn_background= "588157";
             $btn_hover = "#283618";
+            $top_circle = "img/11.png";
         }
 ?>
 
@@ -58,11 +62,15 @@
 </style>
 
 <body dir="rtl" style="color:black ; background-color:#<?php echo $background; ?>;">
+    <img src=<?php echo $top_circle ?> alt="" style="width:280px; height:90px;">
     <br>
-    <h2>سلام و عرض خوش آمد!</h2>
-    <div class="col-6 col-lg-4 mx-auto border border-3 " id="theme" style="padding:12px;">
+    <img src="img/my_pic.png" class="border border-2 border-dark" alt="" style="position: relative;bottom:55px;border-radius: 250px;">
+    
+    <div style="position: relative;bottom:55px;">
+    <h2 style="">سلام خوش اومدی!</h2>
+    <div class="col-9 col-lg-4 mx-auto border border-3 " id="theme" style="padding:12px;">
     <p style="font-size:15px;  ">میتونی تم صفحه رو تغییر بدی!</p>
-    <p style="font-size:15px;">اول یکی رو انتخاب کن</p>
+    <p style="font-size:13px;">اول یکی رو انتخاب کن</p>
     <form method="POST">
         <input type="radio" value="green" name="color" checked="checked">سبز ---
 
@@ -74,6 +82,7 @@
         <input type="submit" value="حالا اینجا کلیک کن!" id="click" style="padding:10px;line-height : 20%;">
     </form>
     </div>
+    
     <br>
     <div id="conect">
     <h5>راه های ارتباطی با من :</h5>
@@ -90,6 +99,7 @@
         <a href="https://api.whatsapp.com/send?phone=989117970185" class="btn" type="button" target="_blank" id="wt" style="color:white;">واتساپ</a>
         <a href="https://t.me/aliwalv" class="btn" type="button" target="_blank" id="wt" style="color:white;">تلگرام</a>
     </p>
+    </div>
     </div>
     <script src="contact.js">
     </script>
